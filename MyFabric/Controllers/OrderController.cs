11aa84ec8796383cb269857904dedc0aa37660c0
mithatsen <mithatsen45@gmail.com>
2021-05-25 21:48:51 +0300

    Order deneme1

---
+++ MyFabric/Controllers/OrderController.cs
@@ -0,0 +1,85 @@
+﻿using Core.DBModels;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyFabric.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFabric.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IOrderItemRepository _orderItemRepository;
+        public OrderController(IOrderRepository orderRepository, IOrderItemRepository orderItemRepository)
+        {
+            _orderRepository = orderRepository;
+            _orderItemRepository = orderItemRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var productType = await _orderRepository.GetAllAsync();
+            return Ok(productType);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetByID(int id)
+        {
+            var productType = await _orderRepository.FindByIdAsync(id);
+            return Ok(productType);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateOrder(CheckOutDto order)
+        {
+            List<OrderItem> orderItems = order.OrderItems;
+            Order orderDetails = new Order();
+
+            if (orderItems != null)
+            {
+                orderDetails.OrderDate = DateTime.Now;
+                orderDetails.DeadLine = order.DeadLine;
+                orderDetails.CustomerID = order.AppUserId;
+                await _orderRepository.AddAsync(orderDetails);
+                var temp = await _orderRepository.GetAllAsync();
+                var LastElementId=temp.Last();
+               foreach (var x in orderItems)
+                {
+                    x.OrderID = LastElementId.ID;
+                    orderDetails.OrderItems.Add(x);
+                    await _orderItemRepository.AddAsync(x);
+                }
+               
+            }
+           
+            return Ok("Eklendi başarıyla");
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateOrder(Order order)
+        {
+            var tempproductTypeRepository = await _orderRepository.FindByIdAsync(order.ID);
+            if (tempproductTypeRepository != null)
+            {
+                await _orderRepository.UpdateAsync(order);
+                return Ok(order);
+            }
+            return BadRequest("Order Bulunamadı");
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteOrder(int id)
+        {
+            var tempproductTypeRepository = await _orderRepository.FindByIdAsync(id);
+            if (tempproductTypeRepository != null)
+            {
+                await _orderRepository.RemoveAsync(id);
+                return Ok("Silme işlemi başarılı");
+            }
+            return BadRequest("Order Bulunamadı");
+        }
+    }
+}
---
+++ MyFabric/DTO/CheckOutDto.cs
@@ -0,0 +1,15 @@
+﻿using Core.DBModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFabric.DTO
+{
+    public class CheckOutDto
+    {
+        public int AppUserId { get; set; }
+        public  List<OrderItem> OrderItems { get; set; }
+        public DateTime? DeadLine { get; set; }
+    }
+}
