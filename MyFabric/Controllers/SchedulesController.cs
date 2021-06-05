832e5521f9af6d67ed7db55f6f287ecac7e81d9c
talha-aggez <66861181+talha-aggez@users.noreply.github.com>
2021-06-05 23:30:38 +0300

    Schedule Süre.

--- MyFabric/Controllers/SchedulesController.cs
+++ MyFabric/Controllers/SchedulesController.cs
@@ -13,72 +13,86 @@ namespace MyFabric.Controllers
     [ApiController]
     public class SchedulesController : ControllerBase
     {
-         
-         private readonly IScheduleRepository _scheduleRepository;
-         public SchedulesController(IScheduleRepository scheduleRepository)
-         {
+
+        private readonly IScheduleRepository _scheduleRepository;
+        private readonly IWorkCenterRepository _workCenterRepository;
+        public SchedulesController(IScheduleRepository scheduleRepository,IWorkCenterRepository workCenterRepository)
+        {
             _scheduleRepository = scheduleRepository;
-         }
-        [HttpGet]
+            _workCenterRepository = workCenterRepository;
+        }
+        [HttpGet("[action]")]
         public async Task<IActionResult> GetScheduleByOrderIdAndProductId(int orderId, int productId)
         {
             var schedules = await _scheduleRepository.GetScheduleByOrderIdAndProductIdAsync(orderId, productId);
             return Ok(schedules);
         }
-        
-         [HttpGet]
-         public async Task<IActionResult> GetAll()
-         {
-             var workCenter = await _scheduleRepository.GetAllAsync();
-             return Ok(workCenter);
-         }
-         [HttpGet("{id}")]
-         public async Task<IActionResult> GetByID(int id)
-         {
-             var workCenter = await _scheduleRepository.FindByIdAsync(id);
-             return Ok(workCenter);
-         }
-         [HttpPost]
-         public async Task<IActionResult> CreateWorkCenter(Schedule workCenter)
-         {
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var workCenter = await _scheduleRepository.GetAllAsync();
+            return Ok(workCenter);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetByID(int id)
+        {
+            var workCenter = await _scheduleRepository.FindByIdAsync(id);
+            return Ok(workCenter);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateWorkCenter(Schedule workCenter)
+        {
             await _scheduleRepository.AddAsync(workCenter);
-             return Ok("Eklendi başarıyla");
-         }
+            return Ok("Eklendi başarıyla");
+        }
         [HttpPost("[action]")]
         public async Task<IActionResult> CreateScheduleByScheduleList(List<ScheduleDto> schedules)
         {
-            foreach (var item in schedules)
+            var scheduleAllList = await _scheduleRepository.GetAllAsync();
+
+
+            foreach (var item2 in schedules)
             {
-                await _scheduleRepository.AddAsync(new Schedule {OrderID=item.OrderID,ProductID=item.ProductID,WorkCenterID=item.WorkCenterID, Speed=item.Speed});
+                var temp = scheduleAllList.Where(p => p.OrderID == item2.OrderID && p.ProductID == item2.ProductID).ToList();
+                if (temp.Count == 0)
+                {
+                    await _scheduleRepository.AddAsync(new Schedule { OrderID = item2.OrderID, ProductID = item2.ProductID, WorkCenterID = item2.WorkCenterID, Speed = item2.Speed });
+                    var workCenter = await _workCenterRepository.FindByIdAsync(item2.WorkCenterID);
+                    workCenter.Active = true;
+                    await _workCenterRepository.UpdateAsync(workCenter);
+                }
+                else
+                {
+                    await _scheduleRepository.UpdateAsync(new Schedule { ID = temp.FirstOrDefault().ID ,OrderID =  item2.OrderID, ProductID = item2.ProductID, WorkCenterID = item2.WorkCenterID, Speed = item2.Speed  });
+                }
             }
 
-
-           
             return Ok("Eklendi başarıyla");
         }
         //deneme
         [HttpPut]
-         public async Task<IActionResult> UpdateWorkCenter(Schedule workCenter)
-         {
-             var tempproductTypeRepository = await _scheduleRepository.FindByIdAsync(workCenter.ID);
-             if (tempproductTypeRepository != null)
-             {
+        public async Task<IActionResult> UpdateWorkCenter(Schedule workCenter)
+        {
+            var tempproductTypeRepository = await _scheduleRepository.FindByIdAsync(workCenter.ID);
+            if (tempproductTypeRepository != null)
+            {
                 await _scheduleRepository.UpdateAsync(workCenter);
-                 return Ok(workCenter);
-             }
-             return BadRequest("Makine Bulunamadı");
-         }
-         [HttpDelete("{id}")]
-         public async Task<IActionResult> DeleteProduct(int id)
-         {
-             var workCenter = await _scheduleRepository.FindByIdAsync(id);
-             if (workCenter != null)
-             {
-                 await _scheduleRepository.RemoveAsync(id);
-                 return NoContent();
-             }
-             return BadRequest("Makine Bulunamadı");
-         }
-       
+                return Ok(workCenter);
+            }
+            return BadRequest("Makine Bulunamadı");
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            var workCenter = await _scheduleRepository.FindByIdAsync(id);
+            if (workCenter != null)
+            {
+                await _scheduleRepository.RemoveAsync(id);
+                return NoContent();
+            }
+            return BadRequest("Makine Bulunamadı");
+        }
+
     }
 }
